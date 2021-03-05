001114c763d9093c8d84301a542ae12ee5e2faf9
Ramla <ramlaali.abd@gmail.com>
2021-03-05 14:13:05 -0500

    commit tree cbc0c1aafcef73e234ae409bafe6a235dcb806c2

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+    array = name_hash.collect {|key, value| value}.sort
+    name_hash.each{|key, value| return key if value == array[0]}
+    return nil
 end
